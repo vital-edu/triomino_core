aae6a6f3a28ff8029e105fecbdbd69c3ffc0326d
Eduardo Vital Alencar Cunha <vital@hey.com>
2021-09-09 18:31:42 -0300

    Create rule book.

---
+++ lib/rule_book.dart
@@ -0,0 +1,9 @@
+import 'package:triomino_core/game_rule.dart';
+
+class RuleBook {
+  final QuantityOfPlayersGameRule quantityOfPlayersGameRule;
+
+  RuleBook({
+    required this.quantityOfPlayersGameRule,
+  });
+}
