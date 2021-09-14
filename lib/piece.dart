930bfe3c252e15d6213a539244a0c292208826ed
Eduardo Vital Alencar Cunha <vital@hey.com>
2021-09-14 18:32:12 -0300

    Change Piece model to have its sides sorted and implement method toString

--- lib/piece.dart
+++ lib/piece.dart
@@ -1,15 +1,30 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
+class Piece {
+  late final int side1;
+  late final int side2;
+  late final int side3;
+  final bool isTriple;
 
-part 'piece.freezed.dart';
+  Piece(int side1, int side2, int side3)
+      : assert(side1 >= 0),
+        assert(side2 >= 0),
+        assert(side3 >= 0),
+        assert(side1 <= 5),
+        assert(side2 <= 5),
+        assert(side3 <= 5),
+        isTriple = side1 == side2 && side1 == side3 {
+    final sortedSides = [side1, side2, side3]..sort();
+    this.side1 = sortedSides[0];
+    this.side2 = sortedSides[1];
+    this.side3 = sortedSides[2];
+  }
 
-@freezed
-class Piece with _$Piece {
-  @Assert('side1 >= 0')
-  @Assert('side2 >= 0')
-  @Assert('side3 >= 0')
-  @Assert('side1 <= 5')
-  @Assert('side2 <= 5')
-  @Assert('side3 <= 5')
-  const factory Piece(int side1, int side2, int side3) = _Piece;
   factory Piece.triple(int side) => Piece(side, side, side);
+
+  @override
+  String toString() {
+    if (isTriple) {
+      return 'Triple $side1';
+    }
+    return 'Piece($side1, $side2, $side3)';
+  }
 }
--- test/lib/piece_test.dart
+++ test/lib/piece_test.dart
@@ -4,13 +4,22 @@ import 'package:triomino_core/piece.dart';
 
 void main() {
   group('default constructor', () {
-    test('sides should not be outside of range [0, 5]', () {
-      expect(() => Piece(-1, 0, 0), throwsA(isA<AssertionError>()));
-      expect(() => Piece(0, -1, 0), throwsA(isA<AssertionError>()));
-      expect(() => Piece(0, 0, -1), throwsA(isA<AssertionError>()));
-      expect(() => Piece(6, 0, 0), throwsA(isA<AssertionError>()));
-      expect(() => Piece(0, 6, 0), throwsA(isA<AssertionError>()));
-      expect(() => Piece(0, 0, 6), throwsA(isA<AssertionError>()));
+    void testInvalidPieceSides(int side1, int side2, int side3) {
+      test('Piece($side1, $side2, $side3) should be an invalid piece', () {
+        expect(
+          () => Piece(side1, side2, side3),
+          throwsA(isA<AssertionError>()),
+        );
+      });
+    }
+
+    group('regular pieces', () {
+      testInvalidPieceSides(-1, 0, 0);
+      testInvalidPieceSides(0, -1, 0);
+      testInvalidPieceSides(0, 0, -1);
+      testInvalidPieceSides(6, 0, 0);
+      testInvalidPieceSides(0, 6, 0);
+      testInvalidPieceSides(0, 0, 6);
     });
 
     test('triples should not be outside of range [0, 5]', () {
@@ -18,10 +27,10 @@ void main() {
       expect(() => Piece.triple(6), throwsA(isA<AssertionError>()));
     });
 
-    test('should create a valid Piece', () {
+    test('should create a valid Piece with sorted sides', () {
       final piece = Piece(1, 0, 5);
-      expect(piece.side1, 1);
-      expect(piece.side2, 0);
+      expect(piece.side1, 0);
+      expect(piece.side2, 1);
       expect(piece.side3, 5);
     });
 
@@ -30,6 +39,29 @@ void main() {
       expect(piece.side1, 5);
       expect(piece.side2, 5);
       expect(piece.side3, 5);
+    });
+  });
+
+  group('toString', () {
+    void testToStringOf(Piece piece, {required String expected}) {
+      test(
+        '${piece.toString()} should be $expected',
+        () => expect(piece.toString(), expected),
+      );
+    }
+
+    group('regular piece', () {
+      testToStringOf(Piece(2, 3, 5), expected: 'Piece(2, 3, 5)');
+      testToStringOf(Piece(2, 5, 3), expected: 'Piece(2, 3, 5)');
+      testToStringOf(Piece(3, 2, 5), expected: 'Piece(2, 3, 5)');
+      testToStringOf(Piece(3, 5, 2), expected: 'Piece(2, 3, 5)');
+      testToStringOf(Piece(5, 2, 3), expected: 'Piece(2, 3, 5)');
+      testToStringOf(Piece(5, 3, 2), expected: 'Piece(2, 3, 5)');
+    });
+
+    group('triple piece', () {
+      testToStringOf(Piece.triple(3), expected: 'Triple 3');
+      testToStringOf(Piece(3, 3, 3), expected: 'Triple 3');
     });
   });
 }
