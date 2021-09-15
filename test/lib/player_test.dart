448fd3eb0935fcb3e05194b1cdd4864a4eabab6e
Eduardo Vital Alencar Cunha <vital@hey.com>
2021-09-14 22:06:22 -0300

    Add Player identity test

---
+++ test/lib/player_test.dart
@@ -0,0 +1,12 @@
+import 'package:test/test.dart';
+import 'package:triomino_core/player.dart';
+
+void main() {
+  group('operator ==', () {
+    test(
+        'different objects with same property value should be considered the same',
+        () {
+      expect(Player(name: 'Player 1'), Player(name: 'Player 1'));
+    });
+  });
+}
